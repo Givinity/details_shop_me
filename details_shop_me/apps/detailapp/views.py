48ced490f9e4db7e4b53551c37159d7018921802
givinity <SaZh.user@gmail.com>
2023-04-17 17:11:01 +0300

    add show categories

--- details_shop_me/apps/detailapp/views.py
+++ details_shop_me/apps/detailapp/views.py
@@ -33,7 +33,8 @@ class IndexView(DataMixin, ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['menu'] = menu
-        c = Category.objects.get(slug=self.kwargs['cat_slug'])
+        cats = Category.objects.all()
+        context['cats'] = cats
         return context
 
     def get_queryset(self):
@@ -53,6 +54,7 @@ class DetailsCategory(ListView):
         context['menu'] = menu
         cats = Category.objects.all()
         context['cats'] = cats
+        context['title'] = 'Категории'
         return context
 
 
